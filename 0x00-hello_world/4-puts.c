996d9a935b99bb687644cc35cd4116c38e486667
Geeky-eddie <edidiongharrison18@gmail.com>
2023-06-17 07:15:33 -0700

    low level language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- * main - entrypoint
- * return - always0
+ * main - entry point
+ *
+ * return:  always 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
