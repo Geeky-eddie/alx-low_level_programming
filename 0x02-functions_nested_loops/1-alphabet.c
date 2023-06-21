37a04d438a41f4ecc976d6954e6f11e502967153
Geeky-eddie <edidiongharrison18@gmail.com>
2023-06-21 13:35:55 -0700

    loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
+}
