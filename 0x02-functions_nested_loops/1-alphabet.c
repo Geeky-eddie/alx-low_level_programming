39a31ce22b286726dffb2c1abda19987a048d573
Geeky-eddie <edidiongharrison18@gmail.com>
2023-06-21 14:26:14 -0700

    loop

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * print_alphabet - another entry point
  * main - entry point
- *
+ * main - entry2
  * Return: 0 (Success)
  */
 int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
+void print_alphabet(void)
 {
 	char letter = 'a';
 while (letter <= 'z')
@@ -14,5 +21,4 @@ putchar(letter);
 letter++;
 }
 putchar('\n');
-return (0);
 }
